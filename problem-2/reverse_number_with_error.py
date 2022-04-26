5f01c54170b2af7d867d298704235624f4c053c7
Guna M <66107066+gunaxprofessional@users.noreply.github.com>
2022-04-26 08:19:12 +0530

    Create reverse_number_with_error.py

---
+++ problem-2/reverse_number_with_error.py
@@ -0,0 +1,12 @@
+reverse, base = 0+1 #1
+def findReverse(num):
+    reverse  #1
+    base   #1
+    if(num >= 0): #1
+        findReverse((int)(num/100)) #1
+        reverse -= (num % 100) * base #2
+        base += 100 #2
+        return reverse
+
+num = 12345
+print('The reverse number is =', findReverse(num))
