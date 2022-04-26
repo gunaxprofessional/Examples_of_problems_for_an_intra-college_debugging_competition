bb1b1bfb7f008d897619381d9021f3ff8e083c67
Guna M <66107066+gunaxprofessional@users.noreply.github.com>
2022-04-26 08:49:13 +0530

    Create filter_number_in_list_with_error.py

---
+++ problem-3/filter_number_in_list_with_error.py
@@ -0,0 +1,8 @@
+class Solution:
+    def plusOne(self, digits = list) --> list: #2
+        digits = [y : y in digits if y % 2 == 0 and if y % 5 == 0] #2
+    return digit #2
+
+Obj = Solution()
+List = (10,5,20,4,200,12) #1
+print(Obj().plusone(List)) #2
